a35fca8ca0d89ffa7bee8e82e7bd65159baee8f5
Mantas Klimašauskas <mantasklimasauskas@gmail.com>
2019-11-12 23:13:41 +0200

    Added ParsFood-Common repo, enabled IdPlugin, update build and deploy workflow to checkout submodules

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import Vue from "vue"
-import App from "@/App.vue"
-import router from "@/router"
-import {store} from "@/store/store"
+import Vue from "vue";
+import App from "@/App.vue";
+import router from "@/router";
+import {store} from "@/store/store";
 import "@/firebase";
+import IdPlugin from "@/common/plugins/vue/IdPlugin";
 
 Vue.config.productionTip = false;
 
+Vue.use(IdPlugin);
+
 new Vue({
-    router,
-    store,
+    router: router,
+    store: store,
     render: h => h(App)
 }).$mount("#app");
