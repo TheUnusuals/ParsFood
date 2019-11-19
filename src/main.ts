08777ea61d0d4e3b613052d31580ee9de09b293e
Mantas Klimašauskas <mantasklimasauskas@gmail.com>
2019-11-19 20:39:51 +0200

    Integrated i18n to the project using vue-i18n package

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+import "reflect-metadata";
+
 import Vue from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import {store} from "@/store/store";
+import {i18n} from "@/i18n";
 import "@/firebase";
 import IdPlugin from "@/common/plugins/vue/IdPlugin";
 
@@ -12,5 +15,6 @@ Vue.use(IdPlugin);
 new Vue({
     router: router,
     store: store,
+    i18n: i18n,
     render: h => h(App)
 }).$mount("#app");
